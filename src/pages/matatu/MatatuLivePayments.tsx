4e85b0dcccf97369f24173f2f4a71d40aa4bfafc
Ayub Kinyua <ayub@skyyalla.com>
2026-01-21 15:08:19 +0300

    Fix unused import in MatatuLivePayments

--- src/pages/matatu/MatatuLivePayments.tsx
+++ src/pages/matatu/MatatuLivePayments.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import DashboardShell from "../../components/DashboardShell";
 import { resolveApiUrl } from "../../services/api";
