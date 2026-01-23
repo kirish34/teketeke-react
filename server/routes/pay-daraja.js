ef304b6e69104391d57332fc5a6939476f61314b
Ayub Kinyua <ayub@skyyalla.com>
2026-01-23 14:16:03 +0300

    Fix pay-daraja duplicate import and add worker script

--- server/routes/pay-daraja.js
+++ server/routes/pay-daraja.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const express = require('express');
 const fetch = require('node-fetch');
 const { supabaseAdmin } = require('../supabase');
 const pool = require('../db/pool');
